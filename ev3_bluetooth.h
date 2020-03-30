29c0b6f965043f90637c7f80c9fd13d3d46116aa
Sofia <gn17o@yandex.ru>
2020-03-30 21:27:44 +0300

    bluetooth keil update

---
+++ ev3_bluetooth.h
@@ -0,0 +1,17 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define ev3_constants_h 
+#define ev3_constants_h
+#ifndef input-output via serial port Keil
+#define input-output via serial port Keil
+
+#define SYSTEM_REPLY 0x03 // System command reply OK
+#define SYSTEM_REPLY_ERROR 0x05 // System command reply ERROR
+#include <stdint.h>
+#include <ev3.h>
+#include <ev3_output.h>
+#include <REG52_H_for_keil.h>
+
+#endif
+}
---
+++ ev3_command.h
@@ -0,0 +1,21 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ev3_h
+#define SUCCESS 0x00
+#define UNKNOWN_HANDLE 0x01
+#define HANDLE_NOT_READY 0x02
+#define CORRUPT_FILE 0x03
+#define NO_HANDLES_AVAILABLE 0x04
+#define NO_PERMISSION 0x05
+#define ILLEGAL_PATH 0x06
+#define FILE_EXITS 0x07
+#define END_OF_FILE 0x08
+#define SIZE_ERROR 0x09
+#define UNKNOWN_ERROR 0x0A
+#define ILLEGAL_FILENAME 0x0B
+#define ILLEGAL_CONNECTION 0x0C
+LEGO
+#endif
+}
